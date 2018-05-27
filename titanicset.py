0ce70613a4e364b10193a0db80bd3b353f881501
KRITIKA GUPTA <kg9810@gmail.com>
2018-05-27 18:13:46 +0530

    Update titanicset.py

--- titanicset.py
+++ titanicset.py
@@ -63,3 +63,4 @@ test_one["Survived"]=0
 # Set Survived to 1 if Sex equals "female" and print the `Survived` column from `test_one`
 test_one["Survived"][test_one["Sex"]=="female"]=1
 print(test_one["Survived"])
+
