2168f94e06852afe6b2ec6deb2bc46f7ab8593f4
KRITIKA GUPTA <kg9810@gmail.com>
2018-05-27 18:01:32 +0530

    Update titanicset.py

--- titanicset.py
+++ titanicset.py
@@ -37,3 +37,18 @@ print(test.describe())
 print("--------------testing shape--------------")
 print(test.shape)
 
+#checking for children -> if the passenger is a child then that child has more chances of getting saved.
+import sys
+# Create the column Child and assign to 'NaN'
+train["Child"] = float('NaN')
+
+# Assign 1 to passengers under 18, 0 to those 18 or older. Print the new column
+train["Child"][train["Age"]<18.0]=1
+train["Child"][train["Age"]>=18.0]=0
+print(train["Child"])
+
+# Print normalized Survival Rates for passengers under 18
+print(train["Survived"][train["Child"] == 1].value_counts(normalize = True))
+
+# Print normalized Survival Rates for passengers 18 or older
+print(train["Survived"][train["Child"] == 0].value_counts(normalize = True))
