3697ff67dbeec6299a8ed5acf289751de08e5758
KRITIKA GUPTA <kg9810@gmail.com>
2018-05-27 18:02:10 +0530

    Update titanicset.py

--- titanicset.py
+++ titanicset.py
@@ -52,3 +52,14 @@ print(train["Survived"][train["Child"] == 1].value_counts(normalize = True))
 
 # Print normalized Survival Rates for passengers 18 or older
 print(train["Survived"][train["Child"] == 0].value_counts(normalize = True))
+
+# Create a copy of test: test_one
+test_one=test
+print(test_one)
+
+# Initialize a Survived column to 0
+test_one["Survived"]=0
+
+# Set Survived to 1 if Sex equals "female" and print the `Survived` column from `test_one`
+test_one["Survived"][test_one["Sex"]=="female"]=1
+print(test_one["Survived"])
